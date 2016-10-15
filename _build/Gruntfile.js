725cfb59633dee3981b524cf3cab65264a4c36e4
Hugo Peek <hugo@fractal-farming.com>
2016-10-15 16:39:23 +0800

    Change package names
    Remove spaces and hyphens from both normal and lowercase package names. This ensures that the package is displayed properly in the extras grid and conforms with overall MODX naming conventions for packages.

--- _build/Gruntfile.js
+++ _build/Gruntfile.js
@@ -7,39 +7,6 @@ module.exports = function(grunt) {
                 dest: 'config.json'
             }
         },
-        replace: {
-            dist: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'templates_all',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-all") %>'
-                        },{
-                            match: 'templates_basic',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-basic") %>'
-                        },{
-                            match: 'templates_publication',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-publication") %>'
-                        },{
-                            match: 'templates_client',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-client") %>'
-                        },{
-                            match: 'templates_team',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-team") %>'
-                        },{
-                            match: 'templates_testimonial',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-testimonial") %>'
-                        },{
-                            match: 'templates_portfolio',
-                            replacement: '<%= grunt.file.read("src_snippets/template-list-portfolio") %>'
-                        }
-                    ]
-                },
-                files: [
-                    {expand: true, flatten: true, src: ['config.json'], dest: ''}
-                ]
-            }
-        },
         jsonlint: {
             all: {
                 src: ['config.json'],
@@ -54,5 +21,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-merge-json');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-jsonlint');
-    grunt.registerTask('default', ['merge-json', 'replace', 'jsonlint']);
+    grunt.registerTask('default', ['merge-json', 'jsonlint']);
 };
