a9203b03e95764d9bbf26d61914bc17991e0ef82
Hugo Peek <hugo@fractal-farming.com>
2018-11-12 17:41:38 +0800

    Add resolver for making small corrections to db
    First correction is pointing getPage to the correct property set.

---
+++ _build/resolvers/corrections.php
@@ -0,0 +1,45 @@
+<?php
+/** @var array $options */
+/** @var xPDOObject $object */
+if ($object->xpdo) {
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            /** @var modX $modx */
+            $modx =& $object->xpdo;
+
+            $modx->log(xPDO::LOG_LEVEL_ERROR, 'checking in..');
+
+            // Fix getPage property set by assigning its new ID
+            $query = $modx->newQuery('modSnippet');
+            $query->where(array(
+                'name' => 'getPage',
+            ));
+            $query->select('id');
+            $getPageID = $modx->getValue($query->prepare());
+
+            // Save doesn't work in the code below.. Bug?
+            //$getPagePropertySet = $modx->getObject('modElementPropertySet', array(
+            //    'element' => 11,
+            //    'element_class' => 'modSnippet',
+            //    'property_set' => 1,
+            //));
+            //if (is_object($getPagePropertySet)) {
+            //    $getPagePropertySet->set('element', $getPageID);
+            //    $getPagePropertySet->save();
+            //}
+
+            // No, just an edge case. Single field can't be updated when primary key consists of multiple fields.
+
+            // Save with direct PDO query instead
+            $table = $modx->getTableName('modElementPropertySet');
+            $update = $modx->prepare("UPDATE " . $table . " SET `element`='" . $getPageID . "' WHERE `element`=" . 11);
+
+            $update->execute();
+            $modx->log(xPDO::LOG_LEVEL_INFO, '[Romanesco] Corrections successfully applied.');
+
+            break;
+    }
+}
+
+return true;
