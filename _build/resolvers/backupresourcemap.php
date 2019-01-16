6d2e05530856ceb73bf79a4d0b48fc7e4c261e13
Hugo Peek <hugo@fractal-farming.com>
2019-01-16 20:10:20 +0800

    Backup resource map before updating package

---
+++ _build/resolvers/backupresourcemap.php
@@ -0,0 +1,25 @@
+<?php
+/** @var array $options */
+/** @var xPDOObject $object */
+if ($object->xpdo) {
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_UPGRADE:
+            /** @var modX $modx */
+            $modx =& $object->xpdo;
+
+            $assetsPath = $modx->getOption('romanescobackyard.assets_path',null,$modx->getOption('assets_path') . 'components/romanescobackyard/');
+
+            if (is_dir($assetsPath)) {
+                copy($assetsPath . 'resourcemap.php', $assetsPath . 'backup/resourcemap_' . time() . '.php.bak');
+
+                $modx->log(xPDO::LOG_LEVEL_INFO, '[Romanesco] Resource map successfully backed up.');
+
+            } else {
+                $modx->log(xPDO::LOG_LEVEL_ERROR, '[Romanesco] Could not backup resource map!');
+            }
+
+            break;
+    }
+}
+
+return true;
