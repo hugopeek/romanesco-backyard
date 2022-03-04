2572afcf36d0e9bf95a92710840e80c7f865dc21
Hugo Peek <hugo@fractal-farming.com>
2022-03-04 15:16:42 +0800

    Add lexicon for system settings
    WIP. Doesn't work yet because the settings are under another namespace.

---
+++ core/components/romanescobackyard/lexicon/en/setting.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+// System settings
+// @todo: Not working, because the settings are under the romanesco namespace and these settings are only picked up under their own namespace (romanescobackyard).
+// ---------------------------------------------------------------------
+
+$_lang['setting_romanesco.cb_hide_advanced_features'] = 'Hide advanced ContentBlocks features';
+$_lang['setting_romanesco.cb_hide_advanced_features_desc'] = 'Disable some of the more technical settings in ContentBlocks, like link rel attributes. Note that this only hides the settings in the manager. Defaults are still applied and changes can still be made with this configuration setting switched on.';
