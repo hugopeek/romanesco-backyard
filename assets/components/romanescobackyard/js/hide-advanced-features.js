4a587999c24262a7a3651d305a009558dcb8dc73
Hugo Peek <hugo@fractal-farming.com>
2023-04-10 11:01:41 +0800

    Tweak hide advanced features functionality
    - Add setting to hide ContentBlocks fields
    - Change hide_advanced_features into hide_settings
    - Accept list of CB settings to hide
    - Load script inline so system settings can be set as variables directly

--- assets/components/romanescobackyard/js/hide-advanced-features.js
+++ assets/components/romanescobackyard/js/hide-advanced-features.js
@@ -1,9 +1,25 @@
+// LOADED INLINE ALREADY, IN MANAGER AUGMENTATIONS PLUGIN
 // Wait for ContentBlocks to finish loading.
 // Depends on arrive.js library (included in Romanesco theme).
 $(document).arrive("#contentblocks-modal", function() {
     $(document).arrive(".contentblocks-modal-content", function() {
+        let settings = '';
+        let fields = '';
 
-        // Hide link rel settings
-        $("label[for='setting-link_rel']").parent().hide();
+        // Hide settings
+        if (settings) {
+            settings = settings.split(',');
+            for (const name of settings) {
+                $(".contentblocks-modal-field [data-name='" + name + "']").parent().hide();
+            }
+        }
+
+        // Hide CB fields
+        if (fields) {
+            fields = fields.split(',');
+            for (const id of fields) {
+                $("li.tooltip a[data-id='" + id + "']").closest('li').hide();
+            }
+        }
     });
 });
